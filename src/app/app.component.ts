5e37b899ee4ff847f296adc791968e225d5bc6e3
geraldineac <87266809+geraldineac@users.noreply.github.com>
2022-09-18 11:32:50 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  armaduras = [
+    'Mark I',
+    'Mark II',
+    'Mark III',
+    'Mark IV',
+    'Mark V',
+    'Mark VI',
+    'Mark XLII'
+  ];
+  ironman = 'Tony Stark';
+  revelar_identidad = false; // cambiar por true
+  usar = this.armaduras[2];
+  superhero = 'Ironman';
+}
